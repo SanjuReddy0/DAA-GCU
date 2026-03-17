7db43426d093f102d5f4ace2f3ca0e9b2c823747
sanzuu <reddysanju79330@gmail.com>
2026-03-17 08:50:14 +0530

    Update selection_sort.cpp

--- lab 1/selection_sort.cpp
+++ lab 1/selection_sort.cpp
@@ -1,28 +1,27 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
-    int n;
-    cout << "Enter number of elements: ";
-    cin >> n;
-    int a[n];
-    cout << "Enter elements:\n";
-    for(int i = 0; i < n; i++)
-        cin >> a[i];
+    int a[] = {5, 2, 9, 1, 3};
+    int n = 5;
 
-    for(int i = 0; i < n - 1; i++) {
-        int minIndex = i;
-        for(int j = i + 1; j < n; j++) {
-            if(a[j] < a[minIndex])
-                minIndex = j;
+    for (int i = 0; i < n - 1; i++) {
+        int min = i;
+        
+        for (int j = i + 1; j < n; j++) {
+            if (a[j] < a[min]) {
+                min = j;
+            }
         }
-        int temp = a[i];
-        a[i] = a[minIndex];
-        a[minIndex] = temp;
+        swap(a[i], a[min]);
     }
 
-    cout << "Sorted array:\n";
-    for(int i = 0; i < n; i++)
+    cout << "Sorted: ";
+    for (int i = 0; i < n; i++) {
         cout << a[i] << " ";
+    }
+    cout << "\n";
+
     return 0;
 }
